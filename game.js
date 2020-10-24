80bfdfb4aea5acdfb64679c10741185446e9ac94
Alvin Laurent Ertanto <alvin.ertanto@gmail.com>
2020-10-24 17:52:33 +0700

    changed to OOP version

--- game.js
+++ game.js
@@ -1,3 +1,5 @@
+// OOP Version
+
 class Game {
   constructor(player, comp) {
     this.player = player;
@@ -13,22 +15,79 @@ class Game {
     this.playerBox = document.querySelectorAll(".greyBox.playerImage");
   }
 
-  getResult() {
-    const playerChoice = this.player.choice;
-    const compChoice = this.comp.choice;
+  getResult(player, comp) {
+    if (player.choice == comp.choice) return (this.result = "DRAW");
+    if (player.choice == "rock")
+      return comp.choice == "scissor"
+        ? (this.result = "PLAYER 1 WIN")
+        : (this.result = "COM WIN");
+    if (player.choice == "paper")
+      return comp.choice == "rock"
+        ? (this.result = "PLAYER 1 WIN")
+        : (this.result = "COM WIN");
+    if (player.choice == "scissor")
+      return comp.choice == "paper"
+        ? (this.result = "PLAYER 1 WIN")
+        : (this.result = "COM WIN");
+  }
+
+  setPlayerGreyBox(player) {
+    if (player.choice == "rock") {
+      this.playerBox[0].style.backgroundColor = "#c4c4c4";
+    } else if (player.choice == "paper") {
+      this.playerBox[1].style.backgroundColor = "#c4c4c4";
+    } else {
+      this.playerBox[2].style.backgroundColor = "#c4c4c4";
+    }
+  }
+
+  setCompGreyBox(comp) {
+    if (comp.choice == "rock") {
+      this.compBox[0].style.backgroundColor = "#c4c4c4";
+    } else if (comp.choice == "paper") {
+      this.compBox[1].style.backgroundColor = "#c4c4c4";
+    } else {
+      this.compBox[2].style.backgroundColor = "#c4c4c4";
+    }
+  }
+
+  showResult(player, comp) {
+    this.versus.style.color = "#9c835f";
+    this.resultClass.classList.add("result");
+
+    this.textResult.innerHTML = this.result;
+    if (this.result == "DRAW") {
+      this.textResult.style.backgroundColor = "#225c0e";
+    } else {
+      this.textResult.style.backgroundColor = "#4c9654";
+    }
+
+    this.setPlayerGreyBox(player);
+    this.setCompGreyBox(comp);
   }
 
   refresh() {
     this.textResult.innerHTML = "";
-    this.result.classList.remove('result');
+    this.resultClass.classList.remove("result");
 
     for (let i = 0; i < this.compBox.length; i++) {
-      this.playerBox[i].style.backgroundColor = '#9c835f';
-      this.compBox[i].style.backgroundColor = '#9c835f';
+      this.playerBox[i].style.backgroundColor = "#9c835f";
+      this.compBox[i].style.backgroundColor = "#9c835f";
     }
 
-    this.versus.style.color = 'rgb(189,48,46)';
+    this.versus.style.color = "rgb(189,48,46)";
     this.result = null;
+  }
+
+  startGame(player, comp) {
+    // Get Comp choice
+    comp.getCompChoice();
+
+    // Get the game result
+    this.getResult(player, comp);
+
+    // Show the result
+    this.showResult(player, comp);
   }
 }
 
@@ -38,8 +97,8 @@ class Player {
     this.choice = null;
   }
 
-  getPlayerChoice() {
-    
+  getPlayerChoice(choice) {
+    this.choice = choice;
   }
 }
 
@@ -55,3 +114,34 @@ class Comp extends Player {
     return (this.choice = "scissor");
   }
 }
+
+const p1 = new Player("Player");
+const cpu = new Comp("CPU");
+const game = new Game(p1, cpu);
+
+console.log(p1);
+console.log(cpu);
+console.log(game);
+
+// Event Listener if player side click any of the player images
+const click = document.querySelectorAll(".contentImage .player");
+click.forEach((playerimg) => {
+  playerimg.addEventListener("click", () => {
+    // Game can only be played if there's no winner result (null)
+    if (!game.result) {
+      // Get player choice (from the second class of each img), parse with substr
+      const playerChoice = playerimg.className.substr(7, 7);
+
+      // Store player choice
+      p1.getPlayerChoice(playerChoice);
+
+      // Start the game
+      game.startGame(p1, cpu);
+    }
+  });
+});
+
+// Refresh listener
+document
+  .querySelector(".refresh")
+  .addEventListener("click", () => game.refresh());
